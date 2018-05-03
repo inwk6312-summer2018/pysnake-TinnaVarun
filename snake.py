178a5d937cf8ab6d52ba9fc738e1f5e5bb108215
TinnaVarun <varun.tinna@dal.ca>
2018-05-03 05:08:51 -0700

    completed

---
+++ snake.py
@@ -0,0 +1,7 @@
+"""Requirements for the snake game"""
+"""1. Keep the background light in color.
+   2. Snake should be dark in color.
+   3. Snake should move linearly.
+   4. Length of the snake will increase when it will hit the DOT.
+   5. Speed of the snake will increase as per its length.
+   6. The game is over if the snake hit the walls."""
